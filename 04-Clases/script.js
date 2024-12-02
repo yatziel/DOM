8e50ce1eab8dc3485c13e98731188138e04ca5ca
Yatziel Ilhuicatl <ylopez@asmedia.edu.mx>
2024-12-02 16:57:10 -0600

    Iniciamos con las clases en el DOM

---
+++ 04-Clases/script.js
@@ -0,0 +1,12 @@
+/* 
+
+  Accedemos al primer elemento de la lista.
+
+*/
+
+const primerBotana = document.getElementsByTagName("li");
+
+console.log(primerBotana[0]);
+
+
+/* Commit -> Iniciamos con Clases en el DOM */
