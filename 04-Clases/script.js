6573d72d90cdf61f30f07eeb82ee6198c2a2f88f
Yatziel Ilhuicatl <ylopez@asmedia.edu.mx>
2024-12-04 15:53:14 -0600

    Terminamos clases con JS

--- 04-Clases/script.js
+++ 04-Clases/script.js
@@ -4,9 +4,60 @@
 
 */
 
-const primerBotana = document.getElementsByTagName("li");
-
-console.log(primerBotana[0]);
+const botanas = document.getElementsByTagName("li");
 
 
-/* Commit -> Iniciamos con Clases en el DOM */
+/* 
+
+  Propiedad classList
+
+  Accedemos a la lista de clases que tiene un elemento.
+
+    elemento.classList
+
+  - Devuelve un DOM Token List (Listado de clases en el DOM) en tipo array.
+
+*/
+
+console.log( botanas[0].classList);
+console.log( botanas[0].classList[0]); // Acceso a clases a través de su índice.
+
+/* 
+
+  Para agregar clases usamos el método add()
+
+    elemento.classList.add(clase)
+
+  - Nombre de la clase pasa como string entre "" o ''
+
+*/
+
+botanas[1].classList.add("bg-ligthpink");
+
+/* 
+
+  Verificar si existe una clase en un elemento con el método contains()
+
+    elemento.classList.contains(clase)
+
+  - Devuelve true si existe la clase
+  - Devuelve false si no existe la clase.
+  - La clase pasa como string entre "" o ''
+
+*/
+
+console.log( botanas[2].classList.contains("botana") );
+console.log( botanas[2].classList.contains("bg-ligthblue") );
+
+/* 
+
+  Para eliminar una clase usamos el método remove()
+
+    elemento.classList.remove(clase)
+
+  - La clase pasa como un string entre "" o ''
+
+*/
+
+botanas[3].classList.remove("bg-ligthpink");
+botanas[3].classList.remove("botana");
